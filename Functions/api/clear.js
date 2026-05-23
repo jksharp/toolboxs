185f2b8207ba0788e6e9c57c664300ecafd15887
jksharp <jksharp@163.com>
2026-05-23 22:52:00 +0800

    删除多余脚本

--- Functions/api/clear.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestPost(context) {
-  const { MY_KV } = context.env;
-  const keys = await MY_KV.list({ prefix: "tool:" });
-  for (const key of keys.keys) {
-    await MY_KV.delete(key.name);
-  }
-  return Response.json({ success: true });
-}
