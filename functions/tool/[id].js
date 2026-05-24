b4a58bf5d967d011b430f10c0430fb7f4ebc6c02
jksharp <jksharp@163.com>
2026-05-24 22:17:05 +0800

    添加动态库

--- functions/tool/[id].js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet(context) {
-  try {
-    const { MY_KV } = context.env;
-    const html = await MY_KV.get(`page-${context.params.id}`);
-    return html ? new Response(html, { headers: { "Content-Type": "text/html;charset=utf-8" } }) : new Response("404", { status: 404 });
-  } catch {
-    return new Response("error", { status: 500 });
-  }
-}
